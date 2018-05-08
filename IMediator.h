3e8ae25d1b68d0ab5d8a1a7924e491095dd5af71
badprog <lol@lollollollol.lol>
2018-05-08 15:27:33 +0200

    Fix indent

--- IMediator.h
+++ IMediator.h
@@ -14,10 +14,10 @@ class IShip;
 class IMediator
 {
 public:
-	virtual ~IMediator() { std::cout << __FUNCTION__ << std::endl; }; // = 0;
-	virtual void spreadMessage(IShip::ENUM_ORE ore, double latitude, double longitude) 	= 0;
-	virtual void addNewRegistered(std::unique_ptr<IShip> registered) 										= 0;
-	virtual const std::vector<std::unique_ptr<IShip>> &getVectorOfExtractors() const 		= 0;
+	virtual ~IMediator() { std::cout << __FUNCTION__ << std::endl; };
+	virtual void spreadMessage(IShip::ENUM_ORE ore, double latitude, double longitude) = 0;
+	virtual void addNewRegistered(std::unique_ptr<IShip> registered) = 0;
+	virtual const std::vector<std::unique_ptr<IShip>> &getVectorOfExtractors() const = 0;
 
 protected:
 	std::vector<std::unique_ptr<IShip>> _vectorOfExtractors;
